6e9eaa4604da9848b754867f3d4965d3102c150f
Bradw91 <imbrad91@gmail.com>
2022-06-11 17:17:44 +0200

    .

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./intro.css";
-import profile_pic from "../../img/profile_pic.png"
+import profile_pic from "../../img/profile_pic.png";
 
 function Intro() {
   return (
@@ -8,30 +8,29 @@ function Intro() {
       <div className="i-left">
         <div className="i-left-wrapper">
           <h1 className="i-name">Brad Willard</h1>
-					<div className="i-title">
-						<div className="i-title-wrapper">
-							<div className="i-title-item">
-								Expat Software Developer
-							</div>
-							<div className="i-title-item">
-								Expat and Immigration  Consultant
-							</div>
-							<div className="i-title-item">
-								Foreign Language Enthusiast
-							</div>
-						</div>
-					</div>
-					<p className="i-desc">
-						I am a management consultant learning Software Development. Currently living in the Netherlands, and have lived in various countries. I love developing technical capabilities, helping the expat community abroad, and learning languages. I am fluent in English and Korean, and speak a bit of Dutch
-					</p>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Expat Software Developer</div>
+              <div className="i-title-item">
+                Expat and Immigration Consultant
+              </div>
+              <div className="i-title-item">Foreign Language Enthusiast</div>
+            </div>
+          </div>
+          <p className="i-desc">
+            I am a management consultant learning Software Development.
+            Currently living in the Netherlands, and have lived in various
+            countries. I love developing technical capabilities, helping the
+            expat community abroad, and learning languages. I am fluent in
+            English and Korean, and speak a bit of Dutch
+          </p>
         </div>
-				
       </div>
 
       <div className="i-right">
-				<div className="i-bg"></div>
-				<img src={profile_pic} alt="" className="i-img" />
-			</div>
+        <div className="i-bg"></div>
+        <img src={profile_pic} alt="" className="i-img" />
+      </div>
     </div>
   );
 }
